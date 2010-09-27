85484041592e50a5eae4d812d00514bd91be32b9
eggcaker <eggcaker@gmail.com>
2010-09-27 11:18:50 +0800

    added flymake for php ,some shortkey

--- common/shortkeys.el
+++ common/shortkeys.el
@@ -1,3 +1,6 @@
 ;; keyboard remark
 (global-set-key "\M-h" 'delete-backward-char)
-(global-set-key "\C-h" 'delete-backward-word)
+(global-set-key "\C-cG" 'end-of-buffer)
+(global-set-key "\C-cg" 'beginning-of-buffer)
+(global-set-key "\C-c{" 'beginning-of-defun)
+(global-set-key "\C-c}" 'end-of-defun)
--- modules/php/init.el
+++ modules/php/init.el
@@ -1,17 +1,27 @@
 ; php-mode configurations
-
+(require 'flymake)
 (require 'php-mode)
+
+(defun flymake-php-init ()
+  "Use php to check the syntax of the current file."
+  (let* ((temp (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))
+    (local (file-relative-name temp (file-name-directory buffer-file-name))))
+    (list "php" (list "-f" local "-l"))))
+
+(add-to-list 'flymake-err-line-patterns
+ '("\\(Parse\\|Fatal\\) error: +\\(.*?\\) in \\(.*?\\) on line \\([0-9]+\\)$" 3 4 nil 2))
+
+(add-to-list 'flymake-allowed-file-name-masks '("\\.php$" flymake-php-init))
+
+(add-hook 'php-mode-hook (lambda () (flymake-mode 1)))
+(define-key php-mode-map "\C-cep" 'flymake-goto-prev-error)
+(define-key php-mode-map "\C-cen" 'flymake-goto-next-error)
+(define-key php-mode-map "\C-csm" 'flymake-display-err-menu-for-current-line)
+
+
 (setq auto-mode-alist
       (append
-       '(("\\.php$" . php-mode)
-      ("\\.inc$" . php-mode))  auto-mode-alist  ))
-
-(defun check-php-syntax()
-  "Performs a PHP syntax on the current file."
-  (interactive)
-  (compile (concat "php -l -f \"" (buffer-file-name) "\"")))
-
-(define-key php-mode-map "\C-cv" 'check-php-syntax)
+       '(("\\.php$" . php-mode))  auto-mode-alist  ))
 
 (defun run-php()
   "Run a PHP script "
