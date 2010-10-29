d79e740cd5fe450b4956fecb19fda2fc359c9234
eggcaker <eggcaker@gmail.com>
2010-10-29 14:14:21 +0800

    added init.el

---
+++ modules/ecb/init.el
@@ -0,0 +1,5 @@
+;; ecb configuration
+(require 'ecb-autoloads)
+(setq ecb-tip-of-the-day nil)
+(ecb-activate)
+
---
+++ modules/geben/init.el
@@ -0,0 +1,2 @@
+; genben xdebug plugin for emacs
+(autoload 'geben "geben" "PHP debugger on Emacs" t)
