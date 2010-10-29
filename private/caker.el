4bb23e90ed5ed6726b6e4f12eeb63c11d4cf42a8
eggcaker <eggcaker@gmail.com>
2010-10-29 11:57:45 +0800

    format

--- private/caker.el
+++ private/caker.el
@@ -18,32 +18,32 @@
                
 ;; ecb defined 
 (custom-set-variables
-  '(ecb-layout-name "left1")
-  '(ecb-layout-window-sizes (quote (("left1" (0.3076923076923077 . 0.29545454545454547) (0.14102564102564102 . 0.3409090909090909) (0.16666666666666666 . 0.3409090909090909) (0.3076923076923077 . 0.3409090909090909)))))
-  '(ecb-source-path (quote (
-                            ("~/src/brayma" "brayma")
-                            ))))
+ '(ecb-layout-name "left1")
+ '(ecb-layout-window-sizes (quote (("left1" (0.3076923076923077 . 0.29545454545454547) (0.14102564102564102 . 0.3409090909090909) (0.16666666666666666 . 0.3409090909090909) (0.3076923076923077 . 0.3409090909090909)))))
+ '(ecb-source-path (quote (
+			   ("~/src/brayma" "brayma")
+			   ))))
 
-                            (project-def "backend"
-                                 '((basedir          "~/src/brayma/backend/")
-                                   (src-patterns     ("*.php" "*.sql"))
-                                   (ignore-patterns  (".svn" "docs"))
-                                   (tags-file        "/Users/eggcaker/src/brayma/backend/TAGS.emacs")
-                                   (file-list-cache  "/Users/eggcaker/.backend/files")
-                                   (open-files-cache "/Users/eggcaker/.backend/open-files")
-                                   (vcs              svn)
-                                   (ack-args         "--php")
-                                   (shutdown-hook    nil)))
+(project-def "backend"
+	     '((basedir          "~/src/brayma/backend/")
+	       (src-patterns     ("*.php" "*.sql"))
+	       (ignore-patterns  (".svn" "docs"))
+	       (tags-file        "/Users/eggcaker/src/brayma/backend/TAGS.emacs")
+	       (file-list-cache  "/Users/eggcaker/.backend/files")
+	       (open-files-cache "/Users/eggcaker/.backend/open-files")
+	       (vcs              svn)
+	       (ack-args         "--php")
+	       (shutdown-hook    nil)))
 
-                            (project-def "frontend"
-                                 '((basedir          "~/src/brayma/frontend")
-                                   (src-patterns     ("*.php" "*.sql" "*.js" "*.html"))
-                                   (ignore-patterns  (".svn" "docs"))
-                                   (tags-file        "~/src/brayma/frontend/TAGS.emacs")
-                                   (file-list-cache  "~/.frontend")
-                                   (open-files-cache "~/.frontend/open-files")
-                                   (vcs              svn)
-                                   (ack-args         "--php --js --html --sql")
-                                   (shutdown-hook    nil)))
+(project-def "frontend"
+	     '((basedir          "~/src/brayma/frontend")
+	       (src-patterns     ("*.php" "*.sql" "*.js" "*.html"))
+	       (ignore-patterns  (".svn" "docs"))
+	       (tags-file        "~/src/brayma/frontend/TAGS.emacs")
+	       (file-list-cache  "~/.frontend")
+	       (open-files-cache "~/.frontend/open-files")
+	       (vcs              svn)
+	       (ack-args         "--php --js --html --sql")
+	       (shutdown-hook    nil)))
                                                                
 (server-start)
