a5a1106e5917132d628395685f18175f7a6ad2c9
eggcaker <eggcaker@gmail.com>
2011-01-04 11:40:24 +0800

    byte compiled

--- modules/ac/loader.el
+++ modules/ac/loader.el
@@ -1,12 +1,15 @@
 ;; auto complemation
 (require 'auto-complete)
 (require 'auto-complete-config)
-(ac-config-default)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/modules/ac/ac-dict")
+(global-auto-complete-mode t) 
 
+(add-to-list 'ac-modes 'jde-mode)
+(add-to-list 'ac-modes 'java-mode)
+(add-to-list 'ac-modes 'nxml-mode)
 ;; Use C-n/C-p to select candidates
 (define-key ac-complete-mode-map "\C-n" 'ac-next)
 (define-key ac-complete-mode-map "\C-p" 'ac-previous)
 
-(global-auto-complete-mode t)
+
 
