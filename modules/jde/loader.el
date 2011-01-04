26a58215f8a3fcfc9114bf2a369e6d054688f61f
eggcaker <eggcaker@gmail.com>
2011-01-04 11:40:24 +0800

    byte compiled

--- modules/jde/loader.el
+++ modules/jde/loader.el
@@ -1,1 +1,12 @@
 (require 'jde)
+(add-hook 'jde-abbrev-mode-hook 'auto-complete-mode)
+
+
+;;(setq auto-mode-alist
+;;      (append
+;;       (list
+;;        '("*\.java" . auto-complete-mode)
+;;	) auto-mode-alist )
+;;)
+
+
