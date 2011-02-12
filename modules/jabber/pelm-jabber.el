0c4bb864faa70687175cc741cb6badc647cd0c3c
eggcaker <eggcaker@gmail.com>
2011-02-12 22:26:11 +0800

    pelm-org

---
+++ modules/jabber/pelm-jabber.el
@@ -0,0 +1,19 @@
+
+(load "jabber-autoloads")
+
+(custom-set-variables
+ '(jabber-alert-message-hooks (quote (jabber-message-awesome jabber-message-echo jabber-message-scroll)))
+ '(jabber-alert-presence-hooks (quote (jabber-presence-awesome jabber-presence-echo)))
+ '(jabber-avatar-verbose nil)
+ '(jabber-backlog-days 3.0)
+ '(jabber-history-enabled t)
+ '(jabber-roster-show-bindings nil)
+ '(jabber-roster-show-title nil)
+ '(jabber-show-offline-contacts nil)
+ '(jabber-show-resources nil)
+ '(jabber-use-global-history nil)
+ '(jabber-vcard-avatars-publish nil)
+ '(jabber-vcard-avatars-retrieve nil)
+ '(joc-eshell-prompt-newline t)
+ '(scroll-bar-width nil t)
+)
