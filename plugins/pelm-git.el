34db0876e72f80148795a2102ce2d4bd77b5dfd7
eggcaker <eggcaker@gmail.com>
2011-12-05 08:28:01 +0800

    migrate git

---
+++ plugins/pelm-git.el
@@ -0,0 +1,19 @@
+;;; pelm-git.el --- PELM  git
+;;
+;; Copyright (c) 2011-2015 eggcaker
+;;
+;; Authors: eggcaker <eggcaker@gmail.com>
+;; URL: http://caker.me/pelm
+;; Version: 0.5.0
+
+;; This file is not part of GNU Emacs
+
+;;; Code:
+i
+;; load git-emacs
+(require 'git-emacs)
+
+(define-key global-map "\C-cx" gitx)
+
+(provide 'pelm-git)
+;;perlm-git.el ends here
