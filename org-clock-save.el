cf7babb88fe9a87a072c82246043f5f6969f55a9
eggcaker <eggcaker@gmail.com>
2012-02-04 09:20:28 +0800

    ignore

--- org-clock-save.el
+++
@@ -1,3 +0,0 @@
-;; org-persist.el - mac at <2012-02-04 Sat 09:18>
-(setq resume-clock '("/Users/eggcaker/.org-files/pelm.org" . 121))
-(setq stored-clock-history '(("/Users/eggcaker/.org-files/refile.org" . 20) ("/Users/eggcaker/.org-files/dit.org" . 23) ("/Users/eggcaker/.org-files/dit.org" . 339) ("/Users/eggcaker/.org-files/pelm.org" . 28)))
