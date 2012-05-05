e3eaa2b08a5b9cd287ae00f02d2ef655c70f7e62
eggcaker <eggcaker@gmail.com>
2012-05-05 19:31:53 +0800

    scala mode

--- plugins/pelm-scala.el
+++ plugins/pelm-scala.el
@@ -10,6 +10,7 @@
 ;;; Code:
 
 (require 'scala-mode-auto)
+(add-to-list 'auto-mode-alist '("\\.sbt$" . scala-mode))
 (add-hook 'scala-mode-hook
           '(lambda()
              (yas/minor-mode-on)
