28cb3a88c62f989c738675f28b66fe43d929e570
eggcaker <eggcaker@gmail.com>
2012-07-10 12:33:05 +0800

    renabled android-mode

--- plugins/pelm-android.el
+++ plugins/pelm-android.el
@@ -9,8 +9,15 @@
 
 ;;; Code:
 
+ (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  '(android-mode-sdk-dir "/usr/local/Cellar/android-sdk/r20")
+)
 
-
+(require 'android-mode)
 
 (provide 'pelm-android)
 
