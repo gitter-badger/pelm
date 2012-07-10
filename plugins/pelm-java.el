58b2fe4138513477a45af435d2e5f360bb8bf90f
eggcaker <eggcaker@gmail.com>
2012-07-10 15:45:08 +0800

    added jdee and extra libs

--- plugins/pelm-java.el
+++ plugins/pelm-java.el
@@ -11,6 +11,20 @@
 
 (autoload 'gtags-mode "gtags" "" t)
 
+(defun screen-width nil -1)
+(define-obsolete-function-alias 'make-local-hook 'ignore "21.1")
+
+;; cedet 1.1
+(load-file (concat pelm-vendor-dir "cedet/common/cedet.el"))
+(global-ede-mode t)
+
+(add-to-list 'load-path (concat pelm-vendor-dir "jdee/lisp"))
+
+(require 'jde-autoload)
+
+
 (provide 'pelm-java)
 
 ;;; pelm-java.el ends here
+
+
