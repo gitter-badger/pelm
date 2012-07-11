8a040ecac9ef590f8220903b63fe6f091ffba699
eggcaker <eggcaker@gmail.com>
2012-07-11 12:10:19 +0800

    changes

--- plugins/pelm-java.el
+++ plugins/pelm-java.el
@@ -9,18 +9,27 @@
 
 ;;; Code:
 
+
+
 (autoload 'gtags-mode "gtags" "" t)
 
-(defun screen-width nil -1)
-(define-obsolete-function-alias 'make-local-hook 'ignore "21.1")
+(if t
+    (message "disabled jdee")
+  
+    (defun screen-width nil -1)
+    (define-obsolete-function-alias 'make-local-hook 'ignore "21.1")
 
-;; cedet 1.1
-(load-file (concat pelm-vendor-dir "cedet/common/cedet.el"))
-(global-ede-mode t)
+    ;; cedet 1.1
+    (load-file (concat pelm-vendor-dir "cedet/common/cedet.el"))
+    (add-to-list 'load-path (concat pelm-vendor-dir "cedet"))
+    (global-ede-mode t)
+    (add-to-list 'load-path (concat pelm-vendor-dir "jdee/lisp"))
 
-(add-to-list 'load-path (concat pelm-vendor-dir "jdee/lisp"))
+    (require 'jde-autoload)
+    (require 'jde)
+)
 
-(require 'jde-autoload)
+(add-to-list 'auto-mode-alist '("\\.java$" . java-mode))
 
 
 (provide 'pelm-java)
---
+++ plugins/pelm-lisp.el
@@ -0,0 +1,17 @@
+;;; pelm-ac.el --- PELM emacs lisp
+;;
+;; Copyright (c) 2011-2015 eggcaker
+;;
+;; Authors: eggcaker <eggcaker@gmail.com>
+;; URL: http://caker.me/pelm
+
+
+;; This file is not part of GNU Emacs
+
+;;; Code:
+
+
+(add-hook 'emacs-lisp-mode-hook 'highlight-parentheses-mode )
+
+
+(provide 'pelm-lisp)
