0df3086c466b4e692f076652600dbbc57f04bb4e
eggcaker <eggcaker@gmail.com>
2012-07-13 14:56:53 +0800

    added jo-exporter

--- plugins/pelm-blog.el
+++ plugins/pelm-blog.el
@@ -22,52 +22,10 @@
 
 (define-key pelm-keymap "op" 'org-publish-blog)
 
-(require 'org-install)
-(require 'org-publish)
-(require 'htmlize)
-
-(setq org-publish-project-alist
-      '(
-        ("org-caker-me"
-         ;; Path to your org files.
-         :base-directory "~/src/Mine/eggcaker.github.com/_org/"
-         :base-extension "org"
-         :exclude "/files/"
-         
-         ;; Path to your Jekyll project.
-         :publishing-directory "~/src/Mine/eggcaker.github.com/"
-         :blog-publishing-directory "~/src/Mine/eggcaker.github.com/"
-         :site-root "http://caker.me"
-         :jekyll-sanitize-permalinks t
-         :recursive t
-         :publishing-function org-publish-org-to-html
-         :headline-levels 4
-         :html-extension "html"
-         :body-only t ;; Only export section between <body> </body>
-         )
-        ("org-static-caker-me"
-         :base-directory "~/src/Mine/eggcaker.github.com/_org/"
-         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|tgz"
-         ;;:base-extension ".*"
-         :publishing-directory "~/src/Mine/eggcaker.github.com/"
-         :recursive t
-         :publishing-function org-publish-attachment)
-        ("caker-me" :components ("org-caker-me" "org-static-caker-me"))
-        ))
-
-
-(require 'org-jekyll)
-(setq org-jekyll-entry-match "+blog-TODO=\"TODO\"")
-
-(defun pelm-el-sanitizer(s)
-  "Sanitize string S"
-  (if (equal (ob-sanitize-string s) "")
-      (md5 s)
-    (ob-sanitize-string s)))
+(require 'jo-exporter)
 
 
 (provide 'pelm-blog)
 
 
-
-
+;;; pelm-blog ends here
--- pre-init.el
+++ pre-init.el
@@ -1,3 +1,3 @@
 (setq exec-path (split-string 
-                 (concat "/usr/local/bin:~/.oh-my-zsh/bin:" (getenv "PATH"))
+                 (concat "~/.rvm/rubies/ruby-1.9.3-head/bin:/usr/local/bin:~/.oh-my-zsh/bin:" (getenv "PATH"))
                  path-separator))
