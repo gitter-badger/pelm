34cab516857b41fc53737e02acff437e9b703621
eggcaker <eggcaker@gmail.com>
2012-09-06 18:32:22 +0800

    daily commit Thu Sep  6 18:32:17 CST 2012

--- plugins/pelm-misc.el
+++ plugins/pelm-misc.el
@@ -100,6 +100,11 @@
  '(jabber-roster-line-format " %c %-25n %u %-8s (%r)")
  '(jabber-show-offline-contacts nil))
 
+ (defun offlineimap-get-password (host port)
+         (let* ((netrc (netrc-parse (expand-file-name "~/.netrc.gpg")))
+                             (hostentry (netrc-machine netrc host port port)))
+                   (when hostentry (netrc-get hostentry "password"))))
+
 
 (provide 'pelm-misc)
 
