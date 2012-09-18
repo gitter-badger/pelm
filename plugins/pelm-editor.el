12ed926ca4819491c5fbc84fae529854657da818
eggcaker <eggcaker@gmail.com>
2012-09-18 23:50:34 +0800

    extract the evil as a plugin

--- plugins/pelm-editor.el
+++ plugins/pelm-editor.el
@@ -63,38 +63,6 @@
 
 
 
-(setq el-get-evil
-      '((:name evil
-               :after (progn
-                        ))
-        (:name evil-leader)
-        ))
-      
-
-(el-get 'sync (loop for src in el-get-evil collect (el-get-source-name src)))
-
-(evil-mode 1)
-
-
-(evil-define-command pelm/evil-maybe-exit ()
-  :repeat change
-  (interactive)
-  (let ((modified (buffer-modified-p)))
-    (insert "j")
-    (let ((evt (read-event (format "Insert %c to exit insert state" ?j)
-               nil 0.5)))
-      (cond
-       ((null evt) (message ""))
-       ((and (integerp evt) (char-equal evt ?n))
-        (delete-char -1)
-        (set-buffer-modified-p modified)
-        (push 'escape unread-command-events))
-       
-       (t (setq unread-command-events (append unread-command-events
-                          (list evt))))))))
-
-(define-key evil-insert-state-map "j" #'pelm/evil-maybe-exit)
-
 
 
 (defun new_line_before ()
