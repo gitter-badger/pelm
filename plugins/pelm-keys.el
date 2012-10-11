988013b4e067233c7550df0e3925de6ded36d682
eggcaker <eggcaker@gmail.com>
2012-10-11 17:51:31 +0800

    daily commit Thu Oct 11 17:51:31 CST 2012

--- plugins/pelm-keys.el
+++ plugins/pelm-keys.el
@@ -11,14 +11,6 @@
 (global-set-key [(control .)] 'pelm-keymap)
 (define-prefix-command 'pelm-keymap)
 
-;; keyboard remark
-
-(define-key pelm-keymap "G" 'end-of-buffer)
-(define-key pelm-keymap "g" 'beginning-of-buffer)
-(define-key pelm-keymap "{" 'beginning-of-defun)
-(define-key pelm-keymap "}" 'end-of-defun)
-
-
 ;;(windmove-default-keybindings)
 (global-set-key (kbd "\e <left>") 'windmove-left)
 (global-set-key (kbd "\e <down>") 'windmove-down)
@@ -33,19 +25,21 @@
 (define-key pelm-keymap "el" 'downcase-region)
 (define-key pelm-keymap "eu" 'upcase-region)
 
-(defun split_window_3 ()
-  "split window horizontally and go to it"
-  (interactive)
-  (split-window-horizontally)
-  (windmove-right))
-
-
 ;sunrise 
 (define-key pelm-keymap "d" 'sunrise)
 
 (global-set-key  "\M-n" 'scroll-up-command)
 (global-set-key  "\M-p" 'scroll-down-command)
 
+
+;;TODO
+(defun pelm-open-plugin-file
+  "Open the plugin file by short name"
+  (interactive )
+  (find-file (concat pelm-dir  "plugins/"))
+  )
+
+(define-key pelm-keymap "f" 'pelm-open-plugin-file)
 (provide 'pelm-keys)
 
 ;;; pelm-keys.el ends here
