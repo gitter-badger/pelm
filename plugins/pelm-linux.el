02d630f8b30b5a1dfdd6d0a94dcc6f340cf476b3
eggcaker <eggcaker@gmail.com>
2015-01-14 05:57:48 +0800

    change font for linux to m+

--- plugins/pelm-linux.el
+++ plugins/pelm-linux.el
@@ -1,4 +1,6 @@
-;;; pelm-org.el --- PELM configuration for linux
+;;; package --- pelm-linux.el  PELM configuration for linux
+;;; Commentary:
+;; Linux configurations
 ;;
 ;; Copyright (c) 2011-2015 eggcaker
 ;;
@@ -10,6 +12,7 @@
 ;;; Code:
 
 (defun pelm-linux-toggle-fullscreen ()
+  "Toggle full screen for linux."
   (interactive)
   (x-send-client-message
    nil 0 nil "_NET_WM_STATE" 32
@@ -17,8 +20,7 @@
 
 (when (string-match "linux" system-configuration)
   (define-key pelm-keymap "uf" 'pelm-linux-toggle-fullscreen)
-  (set-face-font 'default "mplus-1mn-regular-14")
-  )
+  (set-face-font 'default "M+ 1mn medium-18"))
 
 (provide 'pelm-linux)
-;;; ends pelm-linux.el here
+;;; pelm-linux.el ends here
